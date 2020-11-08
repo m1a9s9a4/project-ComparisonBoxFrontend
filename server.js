a924bd1b2b6d5923fe6d59a5f5c233d9d9b21fb3
masafumi kato <xslash1105@gmail.com>
2020-11-08 12:29:27 +0900

    [add] express

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = process.env.PORT || 8081;
+const app = express();
+
+app.use(express.static(__dirname + '/dist/'));
+app.get(/.*/, function(res, req) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
+app.get(/.*/, function(res, req) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
+app.listen(port);
+
+console.log('server started...');
