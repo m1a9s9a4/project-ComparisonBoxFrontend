2c444bd04fd839aa8e564524de115f043505e1a9
masafumi kato <xslash1105@gmail.com>
2020-11-09 01:28:18 +0900

    tmp

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 8081;
 const app = express();
 
 app.use(express.static(__dirname + '/dist/'));
+console.log("process.env.API_URL");
+console.log(process.env.API_URL);
+console.log(process.env.VUE_APP_API_URL);
 app.get(/.*/, function(res, req) {
     res.sendFile(__dirname + "/dist/index.html");
 });
