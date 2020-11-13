605decb390c7b94fe3152fbdfb49a7e8c2b444e1
masafumi kato <xslash1105@gmail.com>
2020-11-13 09:55:52 +0900

    [add] vue-analytics added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics';
 import router from './router';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
@@ -10,6 +11,10 @@ if (!localStorage.getItem('uid')) {
   const uid = new Date().getTime().toString(16) + Math.floor(10000*Math.random()).toString(16);
   localStorage.setItem('uid', uid);
 }
+
+Vue.use(VueAnalytics, {
+  id: process.env.VUE_APP_GA_ID,
+});
 
 new Vue({
   router,
