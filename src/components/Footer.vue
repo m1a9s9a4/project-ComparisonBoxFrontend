4094d9682a9620dd26bedf9e5869b1f451ab2adc
masafumi kato <xslash1105@gmail.com>
2020-11-14 12:25:41 +0900

    [add] added footer for all pages

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-footer color="primary" padless>
+    <v-row justify="center" no-gutters>
+      <v-btn
+        color="white"
+        text
+        rounded
+        class="my-2"
+        href="/"
+      >
+        トップ
+      </v-btn>
+      <v-btn
+        color="white"
+        text
+        rounded
+        class="my-2"
+        href="/#how-to-use"
+      >
+        使い方
+      </v-btn>      
+      <v-col class="primary py-4 text-center white--text" cols="12">
+        {{ new Date().getFullYear() }} — <strong>COMBOX</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
